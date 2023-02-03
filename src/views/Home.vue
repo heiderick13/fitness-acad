7f86f0702c5dbacdb3826fa902e194418d3e39b2
Marcelo Oliveira <marceloheiderick@outlook.com>
2023-02-03 08:57:47 -0300

    update: responsividade e layout professores

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -69,7 +69,7 @@ export default {
         <h2 class="titulo_atividade">Musculação</h2>
         <p class="descricao_atividade">
           Aqui você encontra os melhores equipamentos de musculação para ficar
-          como o físico pronta(o) para o verão.
+          com o físico pronto para o verão.
         </p>
         <p class="preco_atividade">R$ 149,90</p>
       </li>
@@ -84,8 +84,8 @@ export default {
       <li class="atividade">
         <h2 class="titulo_atividade">Jiu-jitsu</h2>
         <p class="descricao_atividade">
-          Aprenda essa luta que consagrou diversos brasileiros mundo pelo mundo.
-          Temos os melhores instrures por aqui.
+          Aprenda essa luta que consagrou diversos brasileiros pelo mundo.
+          Temos os melhores instrutores aqui.
         </p>
         <p class="preco_atividade">R$ 199,99</p>
       </li>
@@ -93,7 +93,7 @@ export default {
         <h2 class="titulo_atividade">Natação</h2>
         <p class="descricao_atividade">
           Venha fazer esse exercício que além de ser um dos melhores para a saúde
-          e condicionamento físico, ainda é refrescante. (Temos piscina aquecido para o inverno)
+          e condicionamento físico, ainda é refrescante. (Temos piscina aquecida para o inverno)
         </p>
         <p class="preco_atividade">R$ 169,99</p>
       </li>
@@ -122,11 +122,11 @@ export default {
         Estamos localizados na Av taltaltal nº 777
         Chapa Quente - RJ Brasil.
         Estamos no mercado há mais de 10 anos, tendo todo carinho e atenção com nossos alunos, trazendo para eles a auto
-        estima para poder cuidar da sua saúde. Temos diverças modalidades para você se desafiar.
+        estima para poder cuidar da sua saúde. Temos diversas modalidades para você se desafiar.
       </p>
       <P>
-        Temos também professores muito capacitados para auxiliar para nossos alunos, em busca de um ótimo
-        resuldado.
+        Temos também professores muito capacitados para auxiliar nossos alunos, em busca de um ótimo
+        resultado.
       </P>
     </div>
 
@@ -137,45 +137,44 @@ export default {
 
       <div>
         <span class="boxe">
-          Arthur Maliano - 34 anos, profissional há mais de 10 anos na área do boxe. Ele está disposto a ajudar os
-          alunos
-          para emagrece e se defender.
+          <img src="../../images/boxe.jpg" alt="boxe" width="250" height="250" class="boxe">
+          <p>Arthur Maliano - 34 anos, profissional há mais de 10 anos na área do boxe. Ele está disposto a ajudar os
+            alunos
+            para emagrece e se defender.</p>
         </span>
         <span class="bikeindoor">
-          Thiago Gomes - 30 anos, profissional durante 7 anos, já ajudou várias pessoas a atingir sua melhor forma e ter
-          uma vida
-          saudável.
+          <img src="../../images/baker-indoor.jpg" alt="bike indoor" width="250" class="bike indoor">
+          <p>Thiago Gomes - 30 anos, profissional durante 7 anos, já ajudou várias pessoas a atingir sua melhor forma e
+            ter
+            uma vida
+            saudável.</p>
         </span>
 
         <span class="jiujitsu">
-          Carlos Alberto - 44 anos mestre de jiujitsu, tem diversas medalhas de compeonato e está disposto a formar
-          futuros campeões de jiujitsu.
+          <img src="../../images/jiujitsu.jpg" alt="jiujitsu" width="250" class="jiujitsu">
+          <p>Carlos Alberto - 44 anos mestre de jiujitsu, tem diversas medalhas de compeonato e está disposto a formar
+            futuros campeões de jiujitsu.</p>
         </span>
         <span class="musculacao">
-          Rafael Dias - 30 anos, personal com grande conhecimento na área para ajudar as pessoas que estão tendo
-          difilcudades em atingir sua melhor forma.
+          <img src="../../images/musculação.jpg" alt="musculação" width="250" class="musculação">
+          <p>Rafael Dias - 30 anos, personal com grande conhecimento na área para ajudar as pessoas que estão tendo
+            difilcudades em atingir sua melhor forma.</p>
         </span>
 
         <span class="jump">
-          Leticia Ribeiro - 30 anos, professora muito procurada nesta área, por conta de seu trabalho bem
-          delicado para que não venha prejudicar os alunos que tem problema de saúde mas que queira fazer o jump.
+          <img src="../../images/jump.jpg" alt="jump" width="250" class=" jump">
+          <p>Leticia Ribeiro - 30 anos, professora muito procurada nesta área, por conta de seu trabalho bem
+            delicado para que não venha prejudicar os alunos que tem problema de saúde mas que queira fazer o jump.</p>
         </span>
         <span class="natacao">
-          Ricardo Gomes- 25 anos, ele tem um ótimo trabalho de natação ao povo de terceira idade, crianças, tendo a
-          responsabilidade de dar o máximo suporte que o aluno esteja precisando.
+          <img src="../../images/natação.jpg" alt="natação" width="255" class="natação">
+          <p>Ricardo Gomes- 25 anos, ele tem um ótimo trabalho de natação ao povo de terceira idade, crianças, tendo a
+            responsabilidade de dar o máximo suporte que o aluno esteja precisando.</p>
         </span>
       </div>
 
     </div>
 
-    <div class="professores-fotos">
-      <img src="../../images/boxe.jpg" alt="boxe" width="250" height="250" class="boxe">
-      <img src="../../images/baker-indoor.jpg" alt="bike indoor" width="250" class="bike indoor">
-      <img src="../../images/jiujitsu.jpg" alt="jiujitsu" width="250" class="jiujitsu">
-      <img src="../../images/musculação.jpg" alt="musculação" width="250" class="musculação">
-      <img src="../../images/jump.jpg" alt="jump" width="250" class=" jump">
-      <img src="../../images/natação.jpg" alt="natação" width="255" class="natação">
-    </div>
   </section>
   <AcadFooter></AcadFooter>
   <BtnScroll></BtnScroll>
@@ -239,8 +238,15 @@ html {
 }
 
 #professores>p {
-  font-size: 24px;
+  margin-bottom: 2rem;
+  font-size: 26px;
   font-weight: 600;
+}
+
+#professores img {
+  height: 150px;
+  width: 150px;
+  clip-path: circle();
 }
 
 .academia {
@@ -251,15 +257,6 @@ html {
 
 .academia h2 {
   font-size: 2rem;
-}
-
-.professores-fotos {
-  display: flex;
-}
-
-.professores-fotos img {
-  height: 150px;
-  margin: 0;
 }
 
 .nomes {
@@ -275,22 +272,9 @@ html {
 .jump,
 .natacao {
   display: flex;
+  align-items: center;
+  gap: 1.5rem;
   margin-bottom: 1.5rem;
-}
-
-@media screen and (max-width: 1200px) {
-  .professores-fotos {
-    background-color: #202020;
-    flex-direction: column;
-    gap: 1.5rem;
-    align-items: center;
-    padding: 2rem 0;
-  }
-
-  .professores-fotos img {
-    height: auto;
-  }
-
 }
 
 
@@ -316,6 +300,29 @@ html {
 
   .carousel-caption p {
     margin-bottom: 20%;
+  }
+
+  #professores>p {
+    text-align: center;
+  }
+
+  .boxe,
+  .bikeindoor,
+  .jiujitsu,
+  .musculacao,
+  .jump,
+  .natacao {
+    flex-direction: column;
+    text-align: justify;
+  }
+
+  .boxe p,
+  .bikeindoor p,
+  .jiujitsu p,
+  .musculacao p,
+  .jump p,
+  .natacao p {
+    padding: 1.5rem;
   }
 }
 </style>
